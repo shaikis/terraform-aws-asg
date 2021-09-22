72d4a39c2edfe1b01a7bb641640689c8d1f11d97
Ismail Shaik <60819791+shaikis@users.noreply.github.com>
2021-09-22 21:09:54 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "sg_id" {
- value = "aws_security_group.asg_instance_sg.id
+  value = aws_security_group.asg_instance_sg.id
+}
+
+output "asg_id" {
+  value = aws_autoscaling_group.main_asg.id
 }
