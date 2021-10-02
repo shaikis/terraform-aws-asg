392b3bf06f03def78e105049e18b34c5ea11a7b5
Ismail Shaik <60819791+shaikis@users.noreply.github.com>
2021-10-02 20:06:30 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -2,18 +2,8 @@ resource "aws_security_group" "asg_instance_sg" {
   name               =  var.asg_name
   vpc_id             =  var.vpcid
 
-  ingress {
-    from_port        = var.asg_instance_sg_ingress_from_port
-    to_port          = var.asg_instance_sg_ingress_to_port
-    protocol         = var.asg_instance_sg_ingress_to_protocol
-    security_groups  = var.alb_app_http_sg_id
-  }
+  ingress = var.ingress_string
 
-  egress {
-    from_port       = var.asg_instance_sg_egress_from_port
-    to_port         = var.asg_instance_sg_egress_to_port
-    protocol        = var.asg_instance_sg_egress_protocol
-    cidr_blocks     = var.asg_instance_sg_egress_cidr_blocks
-  }
+  egress = var.egress_string
 
 }
